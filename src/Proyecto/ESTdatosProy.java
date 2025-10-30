834688f31e5375df60afcea92391d6372134cd4a
Emily Herrera <hemily2310@gmail.com>
2025-10-30 15:48:57 -0600

    Avance listo

--- src/Proyecto/ESTdatosProy.java
+++ src/Proyecto/ESTdatosProy.java
@@ -8,21 +8,27 @@ import Clases.ManejoBanco;
 import Enums.TipoTramite;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author adrianhidalgo
- */
 public class ESTdatosProy {
 
     public static void main(String[] args) throws Exception {
 
         ManejoBanco BAC = new ManejoBanco();
+        String nameBanco = javax.swing.JOptionPane.showInputDialog("Ingrese el nombre del banco: ");
+        if (nameBanco == null) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Sistema cerrado");
+            System.exit(0);
+        }
+        javax.swing.JOptionPane.showMessageDialog(null, "Bienvenido(a) al sistema del banco:  " + nameBanco + "");
 
         String MainOption = "";
 
         do {
             try {
                 MainOption = javax.swing.JOptionPane.showInputDialog("Menu: \n1. Crear cajas\n2. Crear tiquete\n3. Verificar Cajas\n4. Atender tiquete \n5. Salir.");
+                if (MainOption == null) {
+                    javax.swing.JOptionPane.showMessageDialog(null, "Sistema cerrado");
+                    System.exit(0);
+                }
                 switch (Integer.parseInt(MainOption)) {
                     case 1 ->
                         BAC.configurarCajas();
@@ -32,6 +38,8 @@ public class ESTdatosProy {
                         BAC.mostrarTodasLasCajas();
                     case 4 ->
                         BAC.atenderCaja();
+                    case 5 ->
+                        MainOption = "5";
                     default ->
                         javax.swing.JOptionPane.showMessageDialog(null, "Opcion inválida.");
                 }
@@ -40,8 +48,7 @@ public class ESTdatosProy {
                 javax.swing.JOptionPane.showMessageDialog(null, "Opcion inválida.");
                 MainOption = "0";
             }
-            
-            
+
         } while (Integer.parseInt(MainOption) != 5);
 
     }
